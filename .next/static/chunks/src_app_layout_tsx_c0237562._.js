e374ce6d746fb00b13975d56ea0ce98c6f5348dd
Mireb commercial <mirebcommercial@gmail.com>
2025-05-16 09:26:33 +0100

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1747383991810

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_dist_05476590._.js",
+  "static/chunks/[root-of-the-server]__e150344f._.js",
+  "static/chunks/[root-of-the-server]__8c5b3ecc._.css"
+],
+    source: "dynamic"
+});
